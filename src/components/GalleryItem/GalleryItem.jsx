3ca48aae3ad83dc0bc785fb409abd64f64edcf12
Austin Kammerer <austin4191@gmail.com>
2021-10-23 12:00:57 -0500

    GalleryItem styles and dynamically set size of description element

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
+import "./GalleryItem.css";
 
 export default function GalleryItem({ item, updateGalleryItem }) {
   // TODO img click handler to display description instead
   const [isImgClicked, setIsImgClicked] = useState(false);
-  let personUnit = "person";
+  const [descriptionDims, setDescriptionDims] = useState([]);
+
   const clickHandler = (e) => {
     console.dir(e.target);
+
     switch (e.target.tagName) {
       case "IMG":
         console.log("image");
+        // document.querySelector(".description").style.width = `${picWidth}px`;
+        let picWidthStr = `${document.querySelector("img").width}px`;
+        let picHeightStr = `${document.querySelector("img").height}px`;
+        console.log(picWidthStr);
+        setDescriptionDims([picWidthStr, picHeightStr]);
         setIsImgClicked(!isImgClicked);
         break;
       case "BUTTON":
@@ -21,17 +29,30 @@ export default function GalleryItem({ item, updateGalleryItem }) {
         break;
     }
   };
-  console.log(isImgClicked);
+
   // TODO like button click handler to increase likes by 1 (updateGalleryItem)
   return (
-    <div>
-      <img src={item.path} onClick={clickHandler} />
-      <p onClick={clickHandler}>{item.description}</p>
-      <p>
-        {item.likes}{" "}
-        {item.likes === 1 ? "person likes this" : "people like this"}
-      </p>
-      <button onClick={clickHandler}>Like</button>
+    <div className="card">
+      <div className="cardMain">
+        {!isImgClicked ? (
+          <img className="image " src={item.path} onClick={clickHandler} />
+        ) : (
+          <p
+            className="description"
+            onClick={clickHandler}
+            style={{ width: descriptionDims[0], height: descriptionDims[1] }}
+          >
+            {item.description}
+          </p>
+        )}
+      </div>
+      <div className="likes">
+        <p>
+          {item.likes}{" "}
+          {item.likes === 1 ? "person likes this" : "people like this"}
+        </p>
+        <button onClick={clickHandler}>Like</button>
+      </div>
     </div>
   );
 }
