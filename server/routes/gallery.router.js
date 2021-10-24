09f34070df8dc5e74dbad76136dddb4e081d6dad
Austin Kammerer <austin4191@gmail.com>
2021-10-24 11:56:59 -0500

    configured pool and GET req database query

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// import pool
+const pool = require("../modules/pool.js");
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
+router.put("/like/:id", (req, res) => {
+  console.log(req.params);
+  const galleryId = req.params.id;
+  for (const galleryItem of galleryItems) {
+    if (galleryItem.id == galleryId) {
+      galleryItem.likes += 1;
     }
-    res.sendStatus(200);
+  }
+  res.sendStatus(200);
 }); // END PUT Route
 
 // GET Route
-router.get('/', (req, res) => {
-    res.send(galleryItems);
+router.get("/", (req, res) => {
+  // log the request's full url
+  console.log(`GET request at ${req.baseUrl}${req.url}`);
+  const queryText = `SELECT * FROM "gallery" ORDER BY "id"`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows); // send the table rows back to the client
+    })
+    .catch((err) => {
+      console.log(`Error making query ${queryText}`, err);
+      res.sendStatus(500);
+    });
 }); // END GET Route
 
 module.exports = router;
